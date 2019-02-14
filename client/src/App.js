5b377bdfe3839c2487e9e8c4d2f8fb274d61e17f
Eduardo Peixe <>
2019-02-14 11:07:04 -0500

    Add drawings and drawingsList components to the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
-import { subscribeToTimer } from './api'
+import DrawingForm from './DrawingForm'
+import DrawingList from './DrawingList'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    subscribeToTimer(timestamp => {
-      this.setState({
-        timestamp
-      })
-    })
-  }
-
-  state = { timestamp: 'No timestamp set yet' }
-
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Drawing App</h2>
-          <p>Current server timestamp is {this.state.timestamp}</p>
+          <DrawingForm />
+          <DrawingList />
         </div>
       </div>
     )
